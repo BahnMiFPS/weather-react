f0b3e4c34523bfea113d52b92e4eafbcb152a7bf
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-02-10 20:04:02 +1100

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const api = {
+		base: `https://api.openweathermap.org/data/2.5/`,
+		key: `f3fb0ae008e50865f5b486176509ae99`,
+	}
+	const [cityName, setCityName] = useState("")
+	const [data, setData] = useState("")
+	const [loading, setLoading] = useState(false)
+	const [errorMessage, setErrorMessage] = useState("")
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		setCityName(e.target[0].value)
+		console.log(data)
+	}
+
+	useEffect(() => {
+		const fetchData = async () => {
+			if (!cityName) return
+			setLoading(true)
+			try {
+				const response = await fetch(
+					`${api.base}weather?q=${cityName}&appid=${api.key}`
+				)
+				const result = await response.json()
+				if (response.ok) {
+					setData(result)
+					setErrorMessage("")
+				} else {
+					setErrorMessage(result.message)
+				}
+			} catch (error) {
+				setErrorMessage(error.message)
+			}
+			setLoading(false)
+		}
+		fetchData()
+		console.log(data)
+	}, [cityName])
+
+	return (
+		<div className="App">
+			<div className="card">
+				<form className="info title" onSubmit={handleSubmit}>
+					<input type="text" placeholder="city name" />
+					<button type="submit">Search</button>
+				</form>
+				{loading ? (
+					<div>loading ...</div>
+				) : (
+					<>
+						{errorMessage ? (
+							<div className="error">{errorMessage}</div>
+						) : (
+							<div>
+								<div className="info">
+									<h2>Temp: {data.main.temp} °C</h2>
+									<h2>Feels like: {data.main.feels_like} °C</h2>
+								</div>
+								<div className="info">
+									<h2>Weather: {data.weather[0].main}</h2>
+									<h2>Humidity: {data.main.humidity}</h2>
+								</div>
+								<div className="info">
+									<h2>Wind Speed: {data.wind.speed}</h2>
+									<h2>Wind Degrees: {data.wind.deg}</h2>
+								</div>
+							</div>
+						)}
+					</>
+				)}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
